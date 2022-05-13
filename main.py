f46a46c06b53be05c0fc51f373ae8567ba8cbfdb
Lucy <tangluyang555@gmail.com>
2022-05-13 15:32:05 +1000

    horizontal boxes detected

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import webbrowser
+from PIL import Image
 
 with open('json-doc/via_project_12May2022_12h14m.json') as json_file:
     via_file = json.load(json_file)
@@ -20,59 +21,139 @@ for i in range(len(shapes)):
 
 ###############  1 image with horizontal flex boxes ###############
 img = filenames[0]
-_box1 = shape_attributes[img][0]
+backgroud_image = Image.open('./image/'+img)
+img_height = backgroud_image.height
+img_width = backgroud_image.width
+broder = 5
 
-
-####################  Only 1 image with 1 box #####################
-img = filenames[0]
-_box1 = shape_attributes[img][0]
+boxes = shape_attributes[img]
+# box_number = len(shape_attributes[img])
 
 index_page = """ 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>One-box-test</title>
+    <title>horizontal-box-test</title>
     <style>
         .image {
           background-repeat: no-repeat;
-        }
-
-        img {
-          position: absolute;
+          background-image: url("image/%s");
+          width: %spx;
+          height: %spx;
+          display: flex;
+          align-items: flex-start;
         }
         
         .grid1 {
-          position: relative;
-          background-color: transparent;
-          top: %spx;
-          left: %spx;
-          height: %spx;
-          width: %spx;
-          margin: 0;
-          padding: 0;
-          border: 5px solid yellow;
+            background-color: transparent;
+            height: %spx;
+            width: %spx;
+            margin-left: %spx;
+            margin-top: %spx;
+            padding: 0;
+            border: %spx solid yellow;
         }
         
+        .grid2 {
+            background-color: transparent;
+            height: %spx;
+            width: %spx;
+            margin-left: %spx;
+            margin-top: %spx;
+            padding: 0;
+            border: %spx solid yellow;
+        }
+        
+        .grid3 {
+            background-color: transparent;
+            height: %spx;
+            width: %spx;
+            margin-left: %spx;
+            margin-top: %spx;
+            padding: 0;
+            border: %spx solid yellow;
+        }
+        
+        .grid4 {
+            background-color: transparent;
+            height: %spx;
+            width: %spx;
+            margin-left: %spx;
+            margin-top: %spx;
+            padding: 0;
+            border: %spx solid yellow;
+        }
     </style>
-    
+
 </head>
 <body>
     <div class="image">
-        <img src="image/%s" alt="dog"/>
         <div class="grid1"></div>
         <div class="grid2"></div>
+        <div class="grid3"></div>
+        <div class="grid4"></div>
     </div>
 </body>
 </html>
-""" % (_box1['y'], _box1['x'], _box1['height'], _box1['width'], img)
+""" % (img, boxes[-1]['x']+boxes[-1]['width'], img_height,
+       boxes[0]['height'], boxes[0]['width'], boxes[0]['x']-broder,               boxes[0]['y']-broder, broder,
+       boxes[1]['height'], boxes[1]['width'], boxes[1]['x']-boxes[0]['x']-boxes[0]['width']-broder, boxes[1]['y']-broder, broder,
+       boxes[2]['height'], boxes[2]['width'], boxes[2]['x']-boxes[1]['x']-boxes[1]['width']-broder, boxes[2]['y']-broder, broder,
+       boxes[3]['height'], boxes[3]['width'], boxes[3]['x']-boxes[2]['x']-boxes[2]['width']-broder, boxes[3]['y']-broder, broder
+       )
+
+
+# ####################  Only 1 image with 1 box #####################
+# img = filenames[0]
+# _box1 = shape_attributes[img][0]
+#
+# index_page = """
+# <!DOCTYPE html>
+# <html lang="en">
+# <head>
+#     <meta charset="UTF-8">
+#     <title>One-box-test</title>
+#     <style>
+#         .image {
+#           background-repeat: no-repeat;
+#         }
+#
+#         img {
+#           position: absolute;
+#         }
+#
+#         .grid1 {
+#           position: relative;
+#           background-color: transparent;
+#           top: %spx;
+#           left: %spx;
+#           height: %spx;
+#           width: %spx;
+#           margin: 0;
+#           padding: 0;
+#           border: 5px solid yellow;
+#         }
+#
+#     </style>
+#
+# </head>
+# <body>
+#     <div class="image">
+#         <img src="image/%s" alt="dog"/>
+#         <div class="grid1"></div>
+#         <div class="grid2"></div>
+#     </div>
+# </body>
+# </html>
+# """ % (_box1['y'], _box1['x'], _box1['height'], _box1['width'], img)
 
 
 ### Write the HTML to one-box-test.html ###
-GET_HTML = "one-box-test.html"
+GET_HTML = "horizontal-boxes-test.html"
 f = open(GET_HTML, 'w')
 f.write(index_page)
 f.close()
 
 ### Run the HTML file one-box-test.html ###
-webbrowser.open("one-box-test.html")
+webbrowser.open("horizontal-boxes-test.html")
